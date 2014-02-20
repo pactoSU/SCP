30296ff6ce7fc467d37ffb4459b34009e30affc6
Anthony Marcheselli <anthony607@gmail.com>
2014-02-19 16:40:04 -0800

    Modify:FuzzySeal (Tags extracted, awaiting insertion)

--- rubySCP.rb
+++ rubySCP.rb
@@ -1,10 +1,34 @@
 # Starts ruby dicom scp, stores files in C:\pkgs
 
 require 'dicom'
+require 'json'
 include DICOM
 port = 2020
 
 class FileHandler
+    def self.save_file(path_prefix, dcm, transfer_syntax, json)
+      # File name is set using the SOP Instance UID:
+      
+	  #`This is how ruby dicom currently saves, should be able to make it send file to database from here
+	  
+	  puts json
+	  #have to add the dicom file to the json and then send that all to the database ,I think
+	  
+	  file_name = dcm.value("0008,0018") || "missing_SOP_UID"
+      extension = ".dcm"
+      folders = Array.new(3)
+      folders[0] = dcm.value("0010,0020") || "PatientID"
+      folders[1] = dcm.value("0008,0020") || "StudyDate"
+      folders[2] = dcm.value("0008,0060") || "Modality"
+      local_path = folders.join(File::SEPARATOR) + File::SEPARATOR + file_name
+      full_path = path_prefix + local_path + extension
+      # Save the DICOM object to disk:
+      dcm.write(full_path, :transfer_syntax => transfer_syntax)
+      message = [:info, " DICOM file saved to: #{full_path}"]
+	  
+	  
+      return message
+    end
     def self.receive_files(path, objects, transfer_syntaxes)
       all_success = true
       successful, too_short, parse_fail, handle_fail = 0, 0, 0, 0
@@ -23,8 +47,12 @@ class FileHandler
           DICOM.logger.level = server_level
           if dcm.read?
             begin
-			messages << [:info, "METHOD OVERRIDDEN"]
-              message = self.save_file(path, dcm, transfer_syntaxes[i])
+			   puts "overwrite"
+			   hash = dcm.to_hash
+			   json = hash.to_json
+			   
+              message = self.save_file(path, dcm, transfer_syntaxes[i], json)
+			 
               successful += 1
             rescue
               handle_fail += 1
